361a8eeff918695c1ac1f6a307c676ec2e888a91
Bodharth Lonkar <b.lonkar@kalebr.com>
2023-02-08 23:44:32 +0400

    code optimized

--- app/models/block_unconfirmed_transaction.rb
+++ app/models/block_unconfirmed_transaction.rb
@@ -7,7 +7,7 @@ class BlockUnconfirmedTransaction < ApplicationRecord
   def self.save_highest_transaction(transactions)
     selected_unconfirmed_transactions = transactions.select { |transaction| (transaction[:confirmations]).zero? }
     largest_unconfirmed_transaction = selected_unconfirmed_transactions.max_by { |transaction| transaction[:size] }
-    t = find_or_initialize_by(block_hash: largest_unconfirmed_transaction[:hash])
+    t = BlockUnconfirmedTransaction.find_or_initialize_by(block_hash: largest_unconfirmed_transaction[:hash])
     t.data = largest_unconfirmed_transaction.slice(:hash, :total, :fees, :inputs, :outputs)
     t.save!
   rescue StandardError
